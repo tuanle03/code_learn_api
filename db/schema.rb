3cdf977eabd21d6b24eeddda21ff3c1e2f3c9832
Tuan Le <letuanvl03@gmail.com>
2023-12-16 21:24:45 +0700

    Add unit tests for the User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_11_200454) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_16_133434) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "linked_object_id"
@@ -53,6 +53,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_11_200454) do
     t.integer "total_view"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "role", default: "user", null: false
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "votes", force: :cascade do |t|
