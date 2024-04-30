e2c60d73cad034fc01727315b1a458960bdceebf
Tuan Le <letuanvl03@gmail.com>
2024-04-30 23:23:44 +0700

    Raise errors for deprecations in Rails configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,7 @@ RSpec.configure do |config|
 
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
+  config.raise_errors_for_deprecations!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
